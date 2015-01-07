51994c3e5ed5c04c1bfa36f0ac367d8dc0fea335
Juanito Fatas <katehuang0320@gmail.com>
2015-01-07 17:36:16 +0800

    Need to require dependencies.

--- lib/datetime_input.rb
+++ lib/datetime_input.rb
@@ -1,5 +1,8 @@
 require 'datetime_input/version'
 
 module DatetimeInput
-  class Engine < ::Rails::Engine; end
+  class Engine < ::Rails::Engine
+    require 'bootstrap3-datetimepicker-rails'
+    require 'momentjs-rails'
+  end
 end
