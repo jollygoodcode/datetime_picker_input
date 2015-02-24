6b6f363afcbc34cb8ebc45e21658e5454aa43f70
Juanito Fatas <katehuang0320@gmail.com>
2015-02-24 15:33:43 +0800

    forget about generator.

--- lib/datetime_input.rb
+++ lib/datetime_input.rb
@@ -4,11 +4,5 @@ module DatetimeInput
   class Engine < ::Rails::Engine
     require "bootstrap3-datetimepicker-rails"
     require "momentjs-rails"
-
-    config.generators do |g|
-      g.test_framework :rspec, fixture: false
-      g.assets false
-      g.helper false
-    end
   end
 end
