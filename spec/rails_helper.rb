0bac81cffcfa8a347e1a9e1226fbfd61849f8faf
Juanito Fatas <katehuang0320@gmail.com>
2015-02-28 00:47:32 +0800

    Remove unnecessary things.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,4 @@
 ENV["RAILS_ENV"] = "test"
-require "spec_helper"
 require File.expand_path("../dummy/config/environment", __FILE__)
 require "rspec/rails"
 
@@ -9,4 +8,12 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
+
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
 end
