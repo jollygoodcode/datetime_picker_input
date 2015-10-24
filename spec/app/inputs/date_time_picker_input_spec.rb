da6540f4c6d3e92bfabb7fd45b680dfefca50e75
Winston <winston.yongwei@gmail.com>
2015-10-25 00:13:13 +0800

    Replace `date_format` option with `extra_date_format` so that user can customise format, and date will be parsed and displayed correctly if form fails.
    User should not be able to edit the default display and picker format which must match so that
    frontend's momentjs conversion works properly. Add specs.

---
+++ spec/app/inputs/date_time_picker_input_spec.rb
@@ -0,0 +1,36 @@
+require "rails_helper"
+
+class Event
+  include ActiveModel::Model
+
+  attr_accessor :when
+end
+
+describe "DateTimePickerInput", type: :helper do
+  describe "#input" do
+    def input_for(object, attr_name, options={})
+      helper.simple_form_for object, url: '' do |f|
+        f.input attr_name, options
+      end
+    end
+
+    context "field without value" do
+      let(:obj)  { Event.new }
+
+      it "includes default `date_extra_formats" do
+        output = input_for(obj, :when, as: :date_time_picker)
+        expect(output).to match /data-date-extra-formats=.*YYYY-MM-DD HH:mm.*"/
+      end
+    end
+
+    context "field with value" do
+      let(:obj)  { Event.new(when: time) }
+      let(:time) { Time.current }
+
+      it "includes formatted value" do
+        output = input_for(obj, :when, as: :date_time_picker)
+        expect(output).to match /value="#{Regexp.quote(time.strftime("%Y-%m-%d %H:%M"))}"/
+      end
+    end
+  end
+end
