e0a715bc8f3887a610ce8b327192d16c628df205
choonkeat <choonkeat@gmail.com>
2015-10-26 13:09:15 +0800

    The fix: to add js equivalent of display_pattern to date_extra_formats This is to enable datetimepicker to be able to interpret the input[value] correctly

--- app/inputs/date_time_picker_input.rb
+++ app/inputs/date_time_picker_input.rb
@@ -20,6 +20,9 @@ class DateTimePickerInput < SimpleForm::Inputs::StringInput
     input_html_options[:data] ||= {}
     input_html_options[:data].reverse_merge!(date_format: picker_pattern)
 
+    input_html_options[:data][:date_extra_formats] ||= []
+    input_html_options[:data][:date_extra_formats] << picker_pattern
+
     input_html_options[:value] ||= I18n.localize(attr_value.utc, format: display_pattern) if attr_value.present?
 
     template.content_tag :div, class: "input-group date datetime_picker" do
