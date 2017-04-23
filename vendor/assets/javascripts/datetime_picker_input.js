c08671732142c5d78d0fa71b47b59335d58b7989
Winston <winston.yongwei@gmail.com>
2017-04-23 23:38:22 +0800

    Update js to work with Turbolinks 5.

--- vendor/assets/javascripts/datetime_picker_input.js
+++ vendor/assets/javascripts/datetime_picker_input.js
@@ -1,7 +1,7 @@
 //= require moment
 //= require bootstrap-datetimepicker
 
-$(document).on('ready page:change', function() {
+$(document).on('ready page:change turbolinks:load', function() {
   $('input.date_time_picker').datetimepicker();
   $('.input-group.date_time_picker .input-group-btn').on('click', function() {
     $(this).prev('input.date_time_picker').focus().click();
